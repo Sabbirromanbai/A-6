6983b18f56a6a5861cb3ed67a6bda7933890c996
Sabbirromanbai <sabbirbai811@gmail.com>
2026-09-23 21:34:15 +0600

    banner section done

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,9 +26,11 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
+
         <Navbar />
         {children}
         <Footer />
+        
         </body>
     </html>
   );
